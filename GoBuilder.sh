96c252d8411b47d9d0f19d9a013b7cba7608e6c3
Abhishek Kumar <122231597+abhishekumar245@users.noreply.github.com>
2024-07-05 12:56:21 +0530

    Add files via upload

---
+++ GoBuilder.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Go Builder
+# A small bash script for generate Go EXE files on Linux.
+# Author - Abhishek Kumar
+
+clear
+echo "+-------------------------+"
+echo "|╔═╗╔═╗╔╗ ╦ ╦╦╦  ╔╦╗╔═╗╦═╗|"
+echo "|║ ╦║ ║╠╩╗║ ║║║   ║║║╣ ╠╦╝|"
+echo "|╚═╝╚═╝╚═╝╚═╝╩╩═╝═╩╝╚═╝╩╚═|"
+echo "+-------------------------+"
+echo "|----Go EXE File Maker----|"
+echo "+-------------------------+"
+echo -n "Enter File Path of Go File Name with extension: "
+read -e gofile
+
+if [ ! -f "$gofile" ]; then
+    echo "File not found!"
+    exit 1
+fi
+
+echo -n "Enter the save location for the built file: "
+read -e savelocation
+
+if [ ! -d "$savelocation" ]; then
+    echo "Save location is not a valid directory!"
+    exit 1
+fi
+
+filename=$(basename -- "$gofile")
+filename="${filename%.*}"
+
+go build -o "$savelocation/$filename" "$gofile"
+
+if [ $? -eq 0 ]; then
+    echo "Success! Go File Output file created at $savelocation with file name $filename."
+else
+    echo "Failed to build the Go file."
+    exit 1
+fi
